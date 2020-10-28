6fd76d820774958e2ea0ed05cd488b72834afad8
yxxshin <samshin3910@snu.ac.kr>
2020-10-29 01:35:20 +0900

    Finished 02-BST

--- 02-BST/include/BST.hpp
+++ 02-BST/include/BST.hpp
@@ -38,6 +38,7 @@ struct BST
         bool insert(std::unique_ptr<TreeNode<T>>& t, const T& key);
         bool search(std::unique_ptr<TreeNode<T>>& t, const T& key);
         bool remove(std::unique_ptr<TreeNode<T>>& t, const T& key);
+        std::unique_ptr<TreeNode<T>>& FindMaxLSubTree(std::unique_ptr<TreeNode<T>>& t);
 
 };
 
@@ -57,11 +58,42 @@ bool BST<T>::remove(const T& key) {
 }
 
 template <typename T>
+std::unique_ptr<TreeNode<T>>& BST<T>::FindMaxLSubTree(std::unique_ptr<TreeNode<T>>& t){
+    if(t->right == nullptr) {
+        return t;
+    }
+
+    else {
+        return FindMaxLSubTree(t->right);
+    }
+}
+
+template <typename T>
 bool BST<T>::insert(std::unique_ptr<TreeNode<T>>& t, const T& key) {
 
     // TODO
     // if insertion fails (i.e. if the key already exists in tree), return false
     // otherwise, return true
+
+    // insert node
+    if(t == nullptr){
+        t = std::make_unique< TreeNode<T> >(key);
+        return true;
+    }
+
+    // there are nodes
+    else {
+        if(t->element == key)
+            return false;
+        
+        else if(t->element > key){
+            return insert(t->left, key);
+        }
+
+        else {  // if(t->element < key)
+            return insert(t->right, key);
+        }
+    }
 
 }
 
@@ -72,6 +104,21 @@ bool BST<T>::search(std::unique_ptr<TreeNode<T>>& t, const T& key) {
     // if key exists in tree, return true
     // otherwise, return false
 
+    if(t == nullptr){
+        return false;
+    }
+
+    else {
+        if(t->element == key)
+            return true;
+        
+        else if(t->element > key)
+            return search(t->left, key);
+        
+        else
+            return search(t->right, key);
+    }
+
 }
 
 template <typename T>
@@ -81,4 +128,51 @@ bool BST<T>::remove(std::unique_ptr<TreeNode<T>>& t, const T& key) {
     // if key does not exist in tree, return false
     // otherwise, return true
 
+    // not in tree
+    if(t == nullptr)
+        return false;
+
+    // found
+    else if(t->element == key){
+        // 1. is a leaf
+        if(t->left == nullptr && t->right == nullptr){
+            t.reset();
+            return true;
+        }
+    
+        // 2. has one child
+        else if(t->left == nullptr || t->right == nullptr){
+            // has left child
+            if(t->right == nullptr){
+                t = std::move(t->left);
+                return true;
+            }
+
+            // has right child
+            else{   // t->left == nullptr
+                t = std::move(t->right);
+                return true;
+            }
+        }
+
+        // 3. has two children
+        else {
+            // find maximum of L_subtree
+            std::unique_ptr<TreeNode<T>>& SwapNode = FindMaxLSubTree(t->left);
+            t->element = SwapNode->element;
+            SwapNode = std::move(SwapNode->left);
+            return true;
+        }
+    }
+        
+    // didn't found
+    else{
+        // key is bigger than node
+        if(key > t->element){
+            return remove(t->right, key);
+        }
+        else {  // if(t->element < key)
+            return remove(t->left, key);
+        }
+    }
 }
